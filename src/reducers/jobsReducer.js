d87d70e84aebfd2555d6e5421751fbfebd9f8685
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-02 15:13:24 -0400

    Add reducer action types for fetching and adding jobs

--- src/reducers/jobsReducer.js
+++ src/reducers/jobsReducer.js
@@ -1,10 +1,26 @@
+const START_ADDING_JOBS_REQUEST = 'START_ADDING_JOBS_REQUEST'
+const ADD_JOBS = 'ADD_JOBS'
+
 export default function jobsReducer(
   state = {
-    jobs: []
+    jobs: [],
+    requesting: false,
   },
   action
 ) {
   switch(action.type) {
+    case START_ADDING_JOBS_REQUEST:
+      return {
+        ...state,
+        jobs: [...state.jobs],
+        requesting: true
+      }
+    case ADD_JOBS:
+      return {
+        ...state,
+        jobs: action.jobs,
+        requesting: false
+      }
     default:
       return state
   }
