d140d96199fdb66959f3b76ef07e08d3e1e8b776
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-02 15:19:49 -0400

    Create JobSearchInput component for filtering jobs

---
+++ src/components/jobs/JobSearchInput.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class JobSearchInput extends Component {
+  state = {
+    // fill me in later when inputs are made
+  }
+
+  render() {
+    return (
+      <div>
+        JobSearchInput Component
+      </div>
+    )
+  }
+}
+
+export default JobSearchInput
