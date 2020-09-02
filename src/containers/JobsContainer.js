451cab33cff824f55840b29fdc576479eef38974
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-02 15:04:13 -0400

    Add more boilerplate code to JobsContainer

--- src/containers/JobsContainer.js
+++ src/containers/JobsContainer.js
@@ -1,16 +1,40 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import { fetchJobs } from '../actions/fetchJobs'
+
 class JobsContainer extends React.Component {
 
   render() {
     return (
       <div>
-        JobsContainer Component
+        <div>
+          {/* add a navbar using react-bootstrap! */}
+          Navbar Here
+        </div>
+
+        <div>
+          Search here
+        </div>
+
+        <div>
+          List here
+        </div>
       </div>
     )
   }
-
 }
 
-export default JobsContainer
+const mapStateToProps = state => {
+  return {
+    jobs: state.jobs
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getJobs: (url) => dispatch(fetchJobs(url))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(JobsContainer)
