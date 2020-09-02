e1efcb4a066861b119d1be23c53790f3a81e8b63
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-02 12:26:17 -0400

    Add rootReducer with combineReducers for more flexible code

--- src/index.js
+++ src/index.js
@@ -7,14 +7,15 @@ import * as serviceWorker from './serviceWorker';
 
 // redux imports
 import Provider from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 // import reducers below
 import jobsReducer from './reducers/jobsReducer'
 
 // import other components / containers here
 
-const store = createStore(jobsReducer, applyMiddleware(thunk))
+const rootReducer = combineReducers({jobs: jobsReducer})
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
