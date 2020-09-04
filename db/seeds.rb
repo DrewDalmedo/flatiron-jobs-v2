e7bec143117e1ae94c21f03e786ad7b4c88d0b8d
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-04 11:18:32 -0400

    Add basic job data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# jobs
+job_one = Job.new()
+
+job_one.id = SecureRandom.uuid
+job_one.job_type = "Full Time"
+job_one.company = "Sticker Mule"
+job_one.company_url = "https://www.stickermule.com/careers"
+job_one.location = "Anywhere"
+job_one.title = "Site Reliability Engineer"
+job_one.description = "Test Description"
+job_one.how_to_apply = "Apply here: imtotallyavalidlink.yes"
+
+job_one.save()
