ed2a908e337e661b937f0c2354dbac2f33b63a47
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-04 12:01:17 -0400

    Add user create and show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   scope '/api' do
     resources :jobs
+    resources :users, only: [:create, :show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
