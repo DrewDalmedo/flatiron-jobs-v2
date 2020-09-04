9f23623a638db8558a33d8e85a4f38622e2a10fb
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-04 11:10:14 -0400

    Fix route so that resources now appear under /api/[resource here]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
+  scope '/api' do
     resources :jobs
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
