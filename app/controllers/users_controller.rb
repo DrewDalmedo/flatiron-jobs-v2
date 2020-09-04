ce3b81b15420e9302e3a2552a59b461c82c1e4f8
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-04 11:59:23 -0400

    Create Users controller with boilerplate create and show methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  # TODO: add serializer so that you can only see json of essential info (no password hashes!!!)
+
+  def show
+  end
+
+  def create
+  end
+
+end
