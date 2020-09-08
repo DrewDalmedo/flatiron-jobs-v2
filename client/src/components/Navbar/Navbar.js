6b1f61ba39b73fd78756bd3fe3264129b4b863fa
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-08 16:48:43 -0400

    Rewrite Navbar to work with react-router

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink, Link } from 'react-router-dom'
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/container';
 
 
 const Navigation = () => {
   return (
-    <div className="container-fluid">
-      <Navbar sticky="top" bg="primary" variant="dark">
-        <Navbar.Brand href="/">Flatiron Jobs</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/jobs">Jobs</Nav.Link>
-          <Nav.Link href="/profile">Profile</Nav.Link>
-        </Nav>
-      </Navbar>
-    </div>
+    <Navbar expand="lg">
+      <Navbar.Brand>// Flatiron Jobs</Navbar.Brand>
+
+      <Nav className="justify-content-left">
+        <Nav.Item>
+          <Nav.Link>
+            <Link to="/">Home</Link> 
+          </Nav.Link>
+        </Nav.Item>
+
+
+        <Nav.Item>
+          <Nav.Link>
+            <Link to="/jobs">Jobs</Link>
+          </Nav.Link>
+        </Nav.Item>
+
+
+        <Nav.Item>
+          <Nav.Link>
+            <Link to="/profile">Profile</Link>
+          </Nav.Link>
+        </Nav.Item>
+
+      </Nav>
+    </Navbar> 
   )
 }
 
