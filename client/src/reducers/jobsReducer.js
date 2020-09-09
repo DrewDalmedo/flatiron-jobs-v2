6d3e5c35f66cb958b1f5c8092ea1a06fdea749a0
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-09 13:20:52 -0400

    Add job filtering

--- client/src/reducers/jobsReducer.js
+++ client/src/reducers/jobsReducer.js
@@ -1,9 +1,12 @@
 const START_ADDING_JOBS_REQUEST = 'START_ADDING_JOBS_REQUEST'
+const FILTER_JOBS_BY_TYPE = 'FILTER_JOBS_BY_TYPE'
 const ADD_JOBS = 'ADD_JOBS'
 
 export default function jobsReducer(
   state = {
     jobs: [],
+    filteredJobs: [],
+    jobType: "",
     requesting: false,
   },
   action
@@ -13,12 +16,20 @@ export default function jobsReducer(
       return {
         ...state,
         jobs: [...state.jobs],
+        filteredJobs: [...state.filteredJobs],
         requesting: true
+      }
+    case FILTER_JOBS_BY_TYPE:
+      return {
+        ...state,
+        filteredJobs: action.payload.filteredJobs,
+        jobType: action.payload.jobType
       }
     case ADD_JOBS:
       return {
         ...state,
         jobs: action.jobs,
+        filteredJobs: action.jobs,
         requesting: false
       }
     default:
