665d3b07363e3f68b003165afb63704d3889ea51
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-09 13:21:43 -0400

    Add job type filtering

--- client/src/components/jobs/JobSearchInput.js
+++ client/src/components/jobs/JobSearchInput.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react'
 
-const JobSearchInput = (props) => {
+import { filterJobs } from '../../actions/jobActions'
+
+import { connect } from 'react-redux'
+
+class JobSearchInput extends Component {
+  render(){
     return (
       <div className="job-filter">
 
-        <select>
-          <option>Latest Postings</option>
-          <option>Earliest Postings</option>
-          <option>Most Applicants</option>
+        <select
+        >
+          <option value="latest">Latest Postings</option>
+          <option value="earliest">Earliest Postings</option>
+          <option value="most-applicants">Most Applicants</option>
         </select>
 
-        <select>
-          <option>All</option>
-          <option>Full Time</option>
-          <option>Part Time</option>
-          <option>Contract</option>
+        <select
+          value={this.props.jobType}
+          onChange={ e => {
+            // call filter function here
+            this.props.filterJobs(this.props.jobs, e.target.value)
+          }}
+        >
+          <option value="">All</option>
+          <option value="Full Time">Full Time</option>
+          <option value="Part Time">Part Time</option>
+          <option value="Contract">Contract</option>
         </select>
 
       </div>
     )
+  }
 }
 
-export default JobSearchInput;
+const mapStateToProps = state => {
+  return {
+    jobs: state.job.jobs,
+    jobType: state.job.jobType,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    filterJobs: (jobs, jobType) => dispatch( filterJobs(jobs, jobType) )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(JobSearchInput);
