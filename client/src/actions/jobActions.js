ab1fc036c48ad2b77bee593749053c7453f58521
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-09 13:18:33 -0400

    Create filterJobs action

--- client/src/actions/jobActions.js
+++ client/src/actions/jobActions.js
@@ -6,3 +6,15 @@ export function fetchJobs() {
       .then(jobs => dispatch({ type: 'ADD_JOBS', jobs }))
   }
 }
+
+export const filterJobs = (jobs, jobType) => {
+  return (dispatch) => {
+    dispatch({
+      type: 'FILTER_JOBS_BY_TYPE',
+      payload: {
+        jobType: jobType,
+        filteredJobs: jobType === "" ? jobs : jobs.filter( job => job.job_type === jobType)
+      }
+    })
+  }
+}
