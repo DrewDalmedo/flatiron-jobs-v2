1ee3936ae85a9bf948ae42608d49fdc28e7aef2b
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-09 19:18:20 -0400

    Add home page styling

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import homeImg from './home.jpg'
+
+// react-bootstrap
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button'
+
+// react-router-bootstrap
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Home = (props) => {
   return (
-    <div>
-      Home Component
-    </div>
+    <Container fluid className="" style={{backgroundImage: homeImg}}>
+      <Row className="justify-content-xl-center">
+        <h1>// Flatiron Jobs</h1>
+      </Row>
+      <Row className="justify-content-xl-center">
+        <LinkContainer to="/jobs">
+          <Button>Check out some jobs</Button>
+        </LinkContainer>
+      </Row>
+    </Container>
   )
 }
 
