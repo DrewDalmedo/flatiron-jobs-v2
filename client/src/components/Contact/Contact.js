96dfb8fe379b5715c1950eddc0ae780c9b183d95
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-10 11:14:19 -0400

    Fill out contact page

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -3,7 +3,16 @@ import React from 'react'
 const Contact = (props) => {
   return (
     <div>
-      Contact Component
+      You can find me here:
+      <div>
+        <a href="https://github.com/DrewDalmedo" target="_blank">GitHub</a>
+      </div>
+      <div>
+        <a href="https://twitter.com/DrewDalmedo" target="_blank">Twitter</a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/andrew-dalmedo-2b74b4153" target="_blank">LinkedIn</a>
+      </div>
     </div>
   )
 }
