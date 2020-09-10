5b5de249e8fa6bfb39d04653cf355d05cd86bb16
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-10 13:07:17 -0400

    Add link styling

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -5,13 +5,13 @@ const Contact = (props) => {
     <div>
       You can find me here:
       <div>
-        <a href="https://github.com/DrewDalmedo" target="_blank">GitHub</a>
+        <a className="link" href="https://github.com/DrewDalmedo" target="_blank">GitHub</a>
       </div>
       <div>
-        <a href="https://twitter.com/DrewDalmedo" target="_blank">Twitter</a>
+        <a className="link" href="https://twitter.com/DrewDalmedo" target="_blank">Twitter</a>
       </div>
       <div>
-        <a href="https://www.linkedin.com/in/andrew-dalmedo-2b74b4153" target="_blank">LinkedIn</a>
+        <a className="link" href="https://www.linkedin.com/in/andrew-dalmedo-2b74b4153" target="_blank">LinkedIn</a>
       </div>
     </div>
   )
