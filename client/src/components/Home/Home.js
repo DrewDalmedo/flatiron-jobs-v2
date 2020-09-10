1a89d3895bc0fb85c984ab69cb8188618ffaf272
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-10 12:43:47 -0400

    Import and implement styling

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import homeImg from './home.jpg'
+import './Home.css'
 
 // react-bootstrap
 import Container from 'react-bootstrap/Container'
@@ -11,9 +12,9 @@ import { LinkContainer } from 'react-router-bootstrap'
 
 const Home = (props) => {
   return (
-    <Container fluid className="" style={{backgroundImage: homeImg}}>
+    <Container fluid className="header" style={{backgroundImage: homeImg}}>
       <Row className="justify-content-xl-center">
-        <h1>// Flatiron Jobs</h1>
+        <h1><strong className="slash">//</strong> Flatiron Jobs</h1>
       </Row>
       <Row className="justify-content-xl-center">
         <LinkContainer to="/jobs">
